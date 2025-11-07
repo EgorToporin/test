16cc52177c20eb33a46d4fbd7ed5dd118df7b540
EgorToporin <pivao111@gmail.com>
2025-11-07 21:15:35 +0300

    сделал дз

---
+++ lesson3.py
@@ -0,0 +1,32 @@
+#import math
+
+#result = ((8 + math.sin(math.pi))/((32**6) - 83 + math.cos(2)))
+#print(result) 1
+
+#import math
+#result = ((math.cos(7) + (math.pi**3) + 3)/((6**4) + 12 - math.cos(4)))
+#print(result) 2
+
+#import math
+#result = ((math.cos(8**math.pi))/((6**2) -(3**4) + 8*math.pi))
+#print(result) 3
+
+#import math 
+#result = ((6 + (8**2) + math.cos(math.pi))/(math.pi + (8**2)))
+#print(result) 4
+
+#import math 
+#result = ((32**2) + (6**4) - math.pi + 2 /(math.pi * 3) - (6**2) - math.cos(math.pi**2))
+#print(result) 5
+
+#import math
+#result = ((math.cos(pi**3))) - math.cin(Q)/(math.cos(sin(8))) - (3**2) + math.pi
+#print(result) неправильно
+
+#import math
+#result = ((math.cos(math.sin(8**2) + math.pi))/(83**2) - (18**2) - math.sin(math.pi**5))
+#print(result) 7 
+
+#import math
+#result = ((math.cos(math.pi**3) - math.sin(2))/(math.cos(math.sin(8)) - (3**2) + math.pi))
+#print(result) 6
