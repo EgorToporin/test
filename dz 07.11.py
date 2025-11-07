a19c2a115eafc226983260e6765659e8be8a2d4f
EgorToporin <pivao111@gmail.com>
2025-11-07 19:38:44 +0300

    Create dz 07.11.py

---
+++ dz 07.11.py
@@ -0,0 +1,18 @@
+#import math 
+#result = ((6 + (8**2) + math.cos(math.pi))/(math.pi + (8**2)))
+#print(result) 4
+
+#import math 
+#result = ((32**2) + (6**4) - math.pi + 2 /(math.pi * 3) - (6**2) - math.cos(math.pi**2))
+#print(result) 5
+
+#import math
+#result = ((math.cos(math.pi**3) - math.sin(2))/(math.cos(math.sin(8)) - (3**2) + math.pi))
+#print(result) 6
+
+#import math
+#result = ((math.cos(math.sin(8**2) + math.pi))/(83**2) - (18**2) - math.sin(math.pi**5))
+#print(result) 7
+
+#спросить по поводу скобок 
+
